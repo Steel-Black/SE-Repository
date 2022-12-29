f9690a0582c2452465e1644cf1a9c691207439fe
SteelBlack <de-coste@mail.ru>
2022-12-29 16:19:38 +0300

    SearchEngine beta

--- SearchEngineApp/src/main/java/ru/steelblack/SearchEngineApp/repositories/PageRepository.java
+++ SearchEngineApp/src/main/java/ru/steelblack/SearchEngineApp/repositories/PageRepository.java
@@ -1,12 +1,8 @@
 package ru.steelblack.SearchEngineApp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import ru.steelblack.SearchEngineApp.models.Lemma;
 import ru.steelblack.SearchEngineApp.models.Page;
-
-import java.util.List;
 import java.util.Optional;
 
 
